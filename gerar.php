da9db88dbd71eb3b24983af6c4e69e1bfc3faa7f
Victor Carlquist <victorcarlquist@gmail.com>
2012-11-30 14:46:43 -0200

    adicionado suporte a acentos

--- gerar.php
+++ gerar.php
@@ -2,23 +2,30 @@
 	// shell_exec('date'); /
 	//Victor Carlquist
 	//07/11/12
+	ini_set('error_reporting', E_ALL);
+	//header("Content-Type: text/html; charset=ISO-8859-1", true); 
 	include 'htmlToLatex.php';
 	
-	$setor = $_POST['setor'];
-	$dia = $_POST['dia'];
-	$mes = $_POST['mes'];
-	$ano = $_POST['ano'];
-	$texto = $_POST['texto'];
-	$final = $_POST['final'];
-	$nome_des = $_POST['nome_destinatario'];
-	$nome_rem = $_POST['nome_remetente'];
-	$cargo_rem = $_POST['cargo_remetente'];
-	$assunto = $_POST['assunto'];
+	$setor = str_replace_assoc($replace,$_POST['setor']);
+	$dia = str_replace_assoc($replace,$_POST['dia']);
+	$mes = str_replace_assoc($replace,$_POST['mes']);
+	$ano = str_replace_assoc($replace,$_POST['ano']);
+	$texto = str_replace_assoc($replace,$_POST['texto']);
+	$final = str_replace_assoc($replace,$_POST['final']);
+	$nome_des = str_replace_assoc($replace,$_POST['nome_destinatario']);
+	$nome_rem =str_replace_assoc($replace, $_POST['nome_remetente']);
+	$cargo_rem = str_replace_assoc($replace,$_POST['cargo_remetente']);
+	$assunto = str_replace_assoc($replace,$_POST['assunto']);
+	
+	echo $_POST['assunto'];
+	if($_POST['assunto'] == 'ã')
+		echo "ola";
 	
 	
-	$texto = str_replace_assoc($replace,$texto);
+	//$texto = str_replace_assoc($replace,$texto);
 
 	$s = <<<EOT
+\\nonstopmode
 \\documentclass[12pt,a4paper]{report}
 \\usepackage[brazil]{babel} % Habilita o uso do idioma português do brasil (PT-BR).
 \\usepackage[T1]{fontenc}
@@ -42,12 +49,12 @@ EOT;
 	$fh = fopen($nome_arq, 'w') or die("can't open file");
 	fwrite($fh, $s);
 	fclose($fh);
-	system("xelatex memorando.tex");
+	exec("xelatex memorando.tex");
 	chmod ("memorando.log", 0777); 
 	chmod ("memorando.pdf", 0777); 
 	chmod ("memorando.aux", 0777); 
 	chmod ("missfont.log", 0777); 
-	echo system("xelatex memorando.tex");
+	exec("xelatex memorando.tex");
 	chmod ("memorando.pdf", 0777); 
 	//echo $s;
 	echo "<a href='memorando.pdf'>Download</a>";
